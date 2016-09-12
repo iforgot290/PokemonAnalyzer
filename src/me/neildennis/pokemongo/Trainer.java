1089f7b2a7cc2992f2276dc08213ea969b078351
Neil <iforgot290@gmail.com>
2016-09-12 13:57:23 -0400

    Added more comparisons and stuff

--- src/me/neildennis/pokemongo/Trainer.java
+++ src/me/neildennis/pokemongo/Trainer.java
@@ -1,7 +1,6 @@
 package me.neildennis.pokemongo;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -21,12 +20,21 @@ public class Trainer {
 	
 	public void analyzePokemon(){
 		//List<Pokemon> strongest = new ArrayList<Pokemon>();
+		Pokemon ivstrong = null;
 		
 		for (int id = 1; id < 152; id++){
 			Pokemon strong = null;
 			List<Pokemon> therest = new ArrayList<Pokemon>();
 			
 			for (Pokemon poke : getPokemonsById(id)){
+				if (ivstrong == null){
+					ivstrong = poke;
+				} else {
+					if (ivstrong.getIvInPercentage() < poke.getIvInPercentage()){
+						ivstrong = poke;
+					}
+				}
+				
 				if (strong == null){
 					strong = poke;
 					continue;
@@ -49,15 +57,23 @@ public class Trainer {
 			Main.print("Strongest " + name + " is " + currentcp + " CP current");
 			Main.print("      " + strong.getCpAfterFullEvolve() + " CP after full evolve no powerup");
 			Main.print("      " + strong.getMaxCpFullEvolveAndPowerupForPlayer() + " CP max evolve and powerup");
+			Main.print("      IV Percentage: " + strong.getIvInPercentage());
+			Main.print("      IV ratio: " + strong.getIvRatio());
 			Main.print(" ");
 			
 			for (Pokemon poke : therest){
 				Main.print("Other " + name + " is " + poke.getCp() + " CP current");
 				Main.print("      " + poke.getCpAfterFullEvolve() + " CP after full evolve no powerup");
 				Main.print("      " + poke.getMaxCpFullEvolveAndPowerupForPlayer() + " CP max evolve and powerup");
+				Main.print("      IV Percentage: " + poke.getIvInPercentage());
+				Main.print("      IV ratio: " + poke.getIvRatio());
 				Main.print(" ");
 			}
 		}
+		
+		if (ivstrong == null) return;
+		
+		Main.print("Strongest IV is: " + PokeDictionary.getDisplayName(ivstrong.getPokemonId().getNumber(), Locale.ENGLISH) + " at " + ivstrong.getCp());
 	}
 	
 	private List<Pokemon> getPokemonsById(int id){
